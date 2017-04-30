8d7e01a76a98d41d3b04985936a100a455c27d8a
Alex Simonian <alex.simonian@gmail.com>
2017-04-30 18:09:44 -0500

    Install initial package.json

---
+++ crawler.js
@@ -0,0 +1,9 @@
+// makes HTTP requests
+let request = require('request');
+
+// parse and select HTML elements on the page
+let cheerio = require('cheerio');
+
+// parse URLs
+let URL = require('url-parse');
+
