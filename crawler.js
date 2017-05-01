098e148ae1c75e9355cfdb8cf303273cbecb4917
Alex Simonian <alex.simonian@gmail.com>
2017-04-30 23:43:03 -0500

    Refactor for links

--- crawler.js
+++ crawler.js
@@ -2,9 +2,8 @@ var request = require('request');
 var cheerio = require('cheerio');
 var URL = require('url-parse');
 
-var START_URL = "http://www.arstechnica.com";
-var SEARCH_WORD = "stemming";
-var MAX_PAGES_TO_VISIT = 10;
+var START_URL = "http://www.complex.com/pop-culture/2011/09/25-video-game-sites-you-should-be-following/kotaku";
+var MAX_PAGES_TO_VISIT = 100;
 
 var pagesVisited = {};
 var numPagesVisited = 0;
@@ -46,26 +45,18 @@ function visitPage(url, callback) {
      }
      // Parse the document body
      var $ = cheerio.load(body);
-     var isWordFound = searchForWord($, SEARCH_WORD);
-     if(isWordFound) {
-       console.log('Word ' + SEARCH_WORD + ' found at page ' + url);
-     } else {
-       collectInternalLinks($);
-       // In this short program, our callback is just calling crawl()
-       callback();
-     }
+     collectInternalLinks($);
+	     
+     callback();
+
   });
 }
 
-function searchForWord($, word) {
-  var bodyText = $('html > body').text().toLowerCase();
-  return(bodyText.indexOf(word.toLowerCase()) !== -1);
-}
-
 function collectInternalLinks($) {
-    var relativeLinks = $("a[href^='/']");
-    console.log("Found " + relativeLinks.length + " relative links on page");
-    relativeLinks.each(function() {
+    var absoluteLinks = $("a[href^='http']");
+    console.log("Found " + absoluteLinks.length + " absolute links on page");
+    absoluteLinks.each(function() {
         pagesToVisit.push(baseUrl + $(this).attr('href'));
+        console.log(pagesToVisit);
     });
 }
